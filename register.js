d019870078de7153201d6e14bca3d514c2bd233d
pok1220 <pokkung1220@gmail.com>
2024-08-11 01:10:33 +0700

    first commit

---
+++ register.js
@@ -0,0 +1,33 @@
+document.getElementById("sendData").addEventListener("click", sendForm);
+
+function sendForm(){
+    console.log("Hello");
+    let nameBox=document.getElementById("YournameID").value;
+    let telBox=document.getElementById("ConatactNumberID").value;
+    console.log(nameBox);
+    if(nameBox=="" && telBox==""){
+        alert("ชื่อและเบอร์โทรศัพท์ห้ามเป็นค่าว่าง");
+    }else if(nameBox=="" && telBox!=""){
+        alert("ชื่อห้ามเป็นค่าว่าง");
+    }else if(nameBox!="" && telBox==""){
+        alert("เบอร์โทรศัพท์ห้ามเป็นค่าว่าง");
+    }
+
+    let quantity=parseInt(document.getElementById("Quantity_peopleID").value);
+    console.log(quantity)
+    if(quantity<=0 || quantity>15){
+        alert("กรอกจำนวนคนในช่วง 1-15 คนเท่านั้น")
+    }
+}
+
+
+document.getElementById("planeTicketNeedID").addEventListener("click",buyTicket);
+function buyTicket(){
+    if(document.getElementById("planeTicketNeedID").checked){
+        if (confirm("ต้องการซื้อบัตรโดยสารเครื่องบินเองใช่หรือไม")) {
+            alert("คุณเลือกซื้อบัตรโดยสารเครื่องบินเอง")
+          } else {
+            document.getElementById("planeTicketNeedID").checked=false;
+          }
+    }
+}
